22deb139558f2c24d0446e4ccc46c4aef964ffc1
AlbinaDjumagulova <a7586280@gmail.com>
2023-09-01 10:51:56 -0500

    first commit

--- tasks/views.py
+++ tasks/views.py
@@ -5,15 +5,12 @@ from django.shortcuts import render, redirect
 from tasks.models import Task
 
 
-def get_list(request):
-    if request.method == 'GET':
-        tasks = Task.objects.all()
-        return render(request, 'index.html', {"tasks": tasks})
+
 
 
 def get_list (request):
     if request.method == 'GET':
-        tasks = Task.objects.all()
+        tasks = Task.objects.filter(is_active=True).order_by('-id')
         return render(request,'index.html',{"tasks": tasks})
 
 
@@ -21,8 +18,9 @@ def create_task(request):
     if request.method == 'POST':
         title = request.POST.get('title')
         description = request.POST.get('description')
-        create_at = request.POST.get('create_at')
-        type = Task.objects.create(title=title, description=description, create_at=create_at, type=type)
+        created_at = request.POST.get('created_at')
+        type = request.POST.get('type')
+        type = Task.objects.create(title=title, description=description, created_at=created_at, type=type)
         return redirect('index')
     return render(request,'index.html')
 
@@ -31,22 +29,26 @@ def update_task(request,id):
     if request.method == 'POST':
         title = request.POST.get('title')
         description = request.POST.get('discription')
-        create_at= request.POST.get('create_at')
+        created_at= request.POST.get('created_at')
         type= request.POST.get('type')
-        task = Task.odjects.get(id=id)
+        task = Task.odjects.filter(id=id,is_active=True).first()
         task.title=title
         task.description = description
-        task.created_at = create_at
+        task.created_at = created_at
         task.type = type
         task.save()
         return redirect('index')
+    context = {
+        'data':Task.objects.get(id=id)
+    }
     return render(request,'Update.html')
 
 
 def delete_task(request , id):
     if request.method == 'POST':
-        task = Task.objects.get(id=id)
-        task.delete()
+        task = Task.objects.get(id=id,is_active=True)
+        task.is_active = False
+        task.save()
         return redirect('index')
     return  render (request, 'index.html')
 
